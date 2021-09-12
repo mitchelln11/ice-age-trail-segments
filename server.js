4e7ce3386766e932833a51e52930ac283b98960a
Mitchell Naleid <mitchellnaleid@gmail.com>
2021-09-12 12:06:26 -0500

    manitowoc segment and packages attempt with react

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express();
+
+// const http = require('http');
+// const fs = require('fs');
+const port = 3000;
+
+// const server = http.createServer( (req,res) => {
+
+// });
+
+// const server = http.createServer( function(req,res){
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     fs.readFile('./index.html', function(error, data) {
+//         if (error) {
+//             res.writeHead(404);
+//             res.write('Error: File not found');
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();
+//     });
+// });
+
+// server.listen(port, (error) => {
+//     if(error) {
+//         console.log('Something went wrong', error);
+//     } else {
+//         console.log(`Server is running on ${port}`);
+//     }
+// });
+
+// Static Files
+app.use('css', {'Content-Type': 'text/css'}, express.static(`${__dirname}/css/styles.css`));
+app.use('test', express.static(`${__dirname}/test.js`));
+
+app.get('', (req,res) => {
+    res.sendFile(`${__dirname}/index.html` )
+});
+
+app.listen(port, (error) => {
+    if(error) {
+        console.log('Something went wrong', error);
+    } else {
+        console.log(`Server is running on port: ${port}`);
+    }
+});
