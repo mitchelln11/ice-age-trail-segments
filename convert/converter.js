b1ea852e2c02ed5606e8f991e2409e9d35890d64
Mitchell Naleid <mitchellnaleid@gmail.com>
2026-06-13 23:03:55 -0500

    feat: north side of Verona Segment

--- convert/converter.js
+++ convert/converter.js
@@ -70,6 +70,6 @@ function findAllKeys(obj, keyToFind) {
     }
 
     recursiveSearch(obj);
-    return results
-    // return results.reverse() // Use if path walked in reverse direction
+    // return results // Use if path walked in forward direction
+    return results.reverse() // Use if path walked in reverse direction
 }
